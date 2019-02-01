28377e2f967124f812fb626c6ba49bc0e432c01f
Administrator <1031772215@qq.com>
2019-02-01 16:33:06 +0800

    所有功能已完成

--- app/src/main/java/com/sgmdata/weeg/myapplication/currentdata.java
+++ app/src/main/java/com/sgmdata/weeg/myapplication/currentdata.java
@@ -12,6 +12,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sgmdata.weeg.myapplication.DataTypeCollect.CommSGMPStatusDataBean;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xutils.common.Callback;
@@ -24,6 +26,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,7 +39,7 @@ import java.util.Scanner;
 
 public class currentdata extends BasePager {
     ListView currentview=null;
-    String itemtext[]={"表头序号","当前读数","阀门状态","信号强度","电池电压","内嵌版本","上传时间"};
+    String itemtext[]={"表头序号","当前读数","阀门状态","信号强度","电池电压","内嵌版本","客户代码","上传时间"};
     EditText sernum=null;
     Button datasearchbut=null;
     InputStream in=null;
@@ -125,8 +129,193 @@ public class currentdata extends BasePager {
                         content.set(i,map);
                     }
                     va.notifyDataSetChanged();
-                    accesswebservice();
+         //           accesswebservice();
+                    accesswebcurdata();
                     break;
+            }
+        }
+    }
+    private void accesswebcurdata(){
+
+        String testert=sernum.getText().toString();
+        if(testert.isEmpty())
+        {
+            Toast.makeText(mActivity,"请输入表具号",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String url = "http://112.21.191.16:8000/SGMPServices/SgmpInfoInterface.asmx/GetSGMPCurrentData?userid=root&userpasswd=weeg@20080725&sgmpserial=%s";
+        url= String.format(url,testert);
+        Log.d("zl","URL"+url);
+        RequestParams params = new RequestParams(url);
+        params.setHeader("Host","112.21.191.16");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.d("zl",result);
+                xmldecodedemo1(result);
+//                Log.d("zl","count:"+mydata.size());
+
+//                CommStatusBean temp;
+//                temp=mydata.get(0);
+//                Log.d("zl","1"+temp.chunnelserial+temp.devserial);
+                va.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.d("zl","ERROR:"+ex.toString());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+
+    }
+
+    private void xmldecodedemo1(String result) {
+
+        CommSGMPStatusDataBean actimeCommData=null;
+        int event=-1;
+        String elementName=null;
+        XmlPullParser parser = Xml.newPullParser();
+        try {
+            parser.setInput(new StringReader(result));
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+        try {
+            event = parser.getEventType();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+        while (event!= XmlPullParser.END_DOCUMENT) {
+            switch (event) {
+                case XmlPullParser.START_DOCUMENT:
+                    break;
+                case XmlPullParser.START_TAG:
+                    elementName=parser.getName();
+                    if("sgmp_current_data".equals(elementName))
+                    {
+                        actimeCommData=new CommSGMPStatusDataBean();
+                    }
+                    break;
+                case XmlPullParser.END_TAG:
+                    elementName=parser.getName();
+                    if("sgmp_current_data".equals(elementName))
+                    {
+                       // mydata.add(actimeCommData);
+                    }
+                    break;
+                case XmlPullParser.TEXT:
+                    if(parser.getText().indexOf("\n")==-1)
+                    {
+                        if("devserial".equals(elementName))
+                        {
+                            Log.d("zl","devserial="+parser.getText());
+                            Map<String,String> map=content.get(0);
+                            map.remove("infos");
+                            map.put("infos",parser.getText());
+                            content.set(0,map);
+                        }
+                        if("devdata5".equals(elementName))
+                        {
+                            Log.d("zl","devdata5="+parser.getText());
+                            Map<String,String> map=content.get(1);
+                            map.remove("infos");
+                            map.put("infos",parser.getText());
+                            content.set(1,map);
+                        }
+                        if("devdata8".equals(elementName))
+                        {
+                            String temp=parser.getText();
+                            Log.d("zl","devdata8="+temp);
+                            Map<String,String> map=content.get(2);
+                            map.remove("infos");
+                            if(temp.equals("85"))
+                                map.put("infos","阀门开");
+                            else if(temp.equals("153"))
+                                map.put("infos","阀门关");
+                            else
+                                map.put("infos","阀门异常");
+                            content.set(2,map);
+                        }
+                        if("devdata6".equals(elementName))
+                        {
+                            Log.d("zl","devdata6="+parser.getText());
+                            Map<String,String> map=content.get(3);
+                            map.remove("infos");
+                            map.put("infos",parser.getText());
+                            content.set(3,map);
+
+                        }
+                        if("devdata2".equals(elementName))
+                        {
+                            Log.d("zl","devdata2="+parser.getText());
+                            Map<String,String> map=content.get(4);
+                            map.remove("infos");
+                            map.put("infos",parser.getText());
+                            content.set(4,map);
+
+                        }
+                        if("devdata7".equals(elementName))
+                        {
+                            Log.d("zl","devdata7="+parser.getText());
+                            Map<String,String> map=content.get(5);
+                            map.remove("infos");
+                            int tempver = Integer.valueOf(parser.getText());
+                            String tempstr = String.format("%x",tempver);
+                            map.put("infos",tempstr);
+                            content.set(5,map);
+                        }
+                        if("devdata10".equals(elementName))
+                        {
+                            Log.d("zl","devdata10="+parser.getText());
+                            Map<String,String> map=content.get(6);
+                            map.remove("infos");
+                            map.put("infos",parser.getText());
+                            content.set(6,map);
+                        }
+                        if("updatedaytime".equals(elementName))
+                        {
+                            Log.d("zl","updatedaytime="+parser.getText());
+                            Map<String,String> map=content.get(7);
+                            map.remove("infos");
+                            String timeinfo = parser.getText();
+                           // Log.d("zl","before replace "+timeinfo);
+                            int index=timeinfo.indexOf('T');
+                            //timeinfo.replace('T',' ');
+                            byte[] temp= timeinfo.getBytes();
+                            temp[index]=(byte) (' ');
+//                            ByteBuffer byteBuffer ;
+//                            byteBuffer=ByteBuffer.allocate(temp.length);
+//                            byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+//                            byteBuffer.put(temp);
+                            timeinfo="";
+                            for(int i=0;i<temp.length;i++)
+                            {
+                                timeinfo+=(char)temp[i];
+                            }
+                            Log.d("zl","after replace "+timeinfo);
+                            map.put("infos",timeinfo);
+                            content.set(7,map);
+                        }
+                    }
+                    break;
+
+            }
+            try {
+                event = parser.next();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
